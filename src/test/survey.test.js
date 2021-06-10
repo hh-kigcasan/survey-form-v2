62fcca9c6972bb66c0656a890f74dbfe383218ce
Darwin <djmanlapat@gmail.com>
2021-06-10 12:58:44 +0800

    trigger thru push 2

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -12,6 +12,7 @@ describe("Survey Model", function(){
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
 
+
         expect(result).to.equal("Success! Captcha input matched.");
     });
 
